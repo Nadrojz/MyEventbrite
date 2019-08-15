cb1547aa677142b16b4d85d35a8a7ea8cec24f49
Nadrojz <jor.perrin@gmail.com>
2019-08-15 12:17:14 +0200

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-require 'faker'
-
-
 User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
 
-10.times do
-
+5.times do
 	first_name = Faker::Name.first_name
 	last_name = Faker::Name.last_name
-	email = "#{first_name.downcase}.#{last_name.downcase}@yopmail.com"
+	email = "#{first_name}.#{last_name}@yopmail.com"
+	User.create(email: email, password: Faker::Internet.password,
+description: Faker::Lorem.paragraph, first_name: first_name, last_name: last_name)
+end
 
-	User.create(
-		first_name:first_name,
-		last_name:last_name,
-		email:email,
-		description:Faker::Quote.famous_last_words,
-		encrypted_password:Faker::Internet.password(min_length: 8)
-		)
+5.times do
+	e = Event.new(start_date: Faker::Time.forward(days: 23).to_datetime,
+		duration: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60].sample,
+		title: Faker::Book.title, description: Faker::Lorem.paragraph, price: rand(1..1000), location: Faker::Address.city, admin: User.all.sample)
+	until e.save
+		e = Event.new(start_date: Faker::Time.forward(days: 23),
+		duration: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60].sample,
+		title: Faker::Book.title, description: Faker::Lorem.paragraph, price: rand(1..1000), location: Faker::Address.city, admin: User.all.sample)
+	end
+end
 
+5.times do
+	a = Attendance.new(user: User.all.sample, event: Event.all.sample)
+	until a.save
+		a = Attendance.new(user: User.all.sample, event: Event.all.sample)
+	end
 end
